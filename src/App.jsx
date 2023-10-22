5236d3d4956e43a242b0cb5894943a0e29b4df45
rizkihutama <rizkihutama05@gmail.com>
2023-10-22 15:37:16 +0700

    small update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
-import './App.css';
+import Cart from './components/Cart/Cart';
+import Layout from './components/Layout/Layout';
+import Products from './components/Shop/Products';
 
 export default function App() {
-  return <>{'asd'}</>;
+  return (
+    <Layout>
+      <Cart />
+      <Products />
+    </Layout>
+  );
 }
